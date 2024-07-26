dab2985291f53ace76474497a44f4aaca8693cc5
levi <425714149@qq.com>
2024-07-26 16:33:21 +0800

    basic mode

---
+++ src/planning_main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include"process.h"
+
+int main()
+{
+    std::cout<<"planning start!"<<std::endl;
+    Process pro;
+    pro.planProcess();
+    std::cout<<"planning end!2142142142121421"<<std::endl;   
+
+    return 0;
+}
