12c85c5d53f149a605854d7b61334e570c98d572
levi <425714149@qq.com>
2024-07-27 09:38:32 +0800

    add test

---
+++ test/map_test/map_test.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include"pnc_map.h"
+using std::cout, std::endl;
+
+void mapTest()
+{
+    cout<<"this is pnc_map test"<<endl;
+}
+
+int main()
+{
+    mapTest();
+    return 0;
+}
