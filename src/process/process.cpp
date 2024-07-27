2a3f3bbf270c06b29fed3e6ecea590cd2b6e97ce
levi <425714149@qq.com>
2024-07-27 08:43:44 +0800

    添加三方库

--- src/process/process.cpp
+++ src/process/process.cpp
@@ -1,9 +1,19 @@
 #include<iostream>
 #include"process.h"
+#include <Eigen/Dense>
+using Eigen::MatrixXd;
+
 
 void Process::planProcess()
 {
     std::cout<<"this is planning process"<<std::endl;
     my_map.mapInfo();
+
+    MatrixXd m(2,2);
+    m(0,0)=1;
+    m(0,1)=8;
+    m(1,0)=6;
+    m(1,1)=m(1,0)+m(0,1);
+    std::cout<<m<<std::endl;
     std::cout<<"planning success"<<std::endl;
 }
